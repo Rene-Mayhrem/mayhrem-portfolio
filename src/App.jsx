a1b7127ce84bf0745f9a73a63c3145aa2b2043e6
mayhrem <renecruz.0711@gmail.com>
2025-09-02 23:27:08 -0600

    Adding header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import Header from '@/components/Header';
+import Header from './components/Header'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
