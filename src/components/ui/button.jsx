cb5bd496e43267a4653b601ff2bee13786fe4249
mayhrem <renecruz.0711@gmail.com>
2025-09-04 00:17:45 -0600

    Adding button for header

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -1,7 +1,12 @@
+import { Slot } from '@radix-ui/react-slot';
 import React from 'react';
+import { cn } from '../../lib/utils';
+import buttonVariants from '../../lib/buttonVariants';
+
+
 
 const Button = React.forwardRef(({ className, variant, size, asChild = false, ...props}, ref) => {
-    const Comp = asChilda ? Slot : 'button';
+    const Comp = asChild ? Slot : 'button';
     return (
         <Comp
             className = {cn(buttonVariants({ variant, size, className }))}
@@ -12,4 +17,4 @@ const Button = React.forwardRef(({ className, variant, size, asChild = false, ..
 });
 Button.displayName = 'Button';
 
-export { Button, buttonVariants };
+export default Button;
